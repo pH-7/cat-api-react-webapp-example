126bcd04bca9e320d3468c17afbd0f76d994ed10
Pierre-Henry Soria <hi@ph7.me>
2022-08-24 09:59:35 +0930

    appDetails: Clear demo API KEY value

--- src/constants/appDetails.js
+++ src/constants/appDetails.js
@@ -1,7 +1,7 @@
-export const APP_NAME = "React Simple App;";
-export const CONTACT_EMAIL = "hi@ph7.me";
-export const AUTHOR_WEBSITE = "https://ph7.me";
+export const APP_NAME = 'React Simple App;';
+export const CONTACT_EMAIL = 'hi@ph7.me';
+export const AUTHOR_WEBSITE = 'https://ph7.me';
 
 // TheCatAPI's Details
-export const API_URL = "https://api.thecatapi.com/v1/";
-export const API_KEY = "cc87643e-9491-43ec-a884-5bfb536d96c4";
+export const API_URL = 'https://api.thecatapi.com/v1/';
+export const API_KEY = '';
